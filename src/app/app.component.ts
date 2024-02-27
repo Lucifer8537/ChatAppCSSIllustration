c9017ce7a0c017dbc9341221e1e945b742c0fb08
navodit <navodit50@gmail.com>
2024-02-27 20:20:47 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'chat-app-css-illustration';
+  title = 'Simple booking';
+  details =
+    'Stay in touch wit our dog walkers through the chat interface. This makes it easy to discuss arrangements and make bookings. Once the walk has been completed you can rate your walker and book again all through the chat.';
 }
